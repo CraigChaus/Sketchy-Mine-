8ba209806486ceb9dab85b9e5863472474f584dc
VS498899 <498899@student.saxion.nl>
2022-02-14 17:46:30 +0100

    added a very simple canvas and toolbox

---
+++ packages/frontend/src/Canvas/Canvas.svelte
@@ -0,0 +1,9 @@
+<script>
+
+</script>
+
+<main>
+    <div>
+        <canvas id="drawingCanvas" width="100%" height="100%" style="border: dashed"> canvas not supported by browser</canvas>
+    </div>
+</main>
---
+++ packages/frontend/src/pages/Main.svelte
@@ -0,0 +1,19 @@
+<script>
+
+import Canvas from "../Canvas/Canvas.svelte";
+import Toolbox from "../Canvas/Toolbox.svelte";
+</script>
+
+<main>
+    <div class="w-full flex flex-col ">
+        <div class="w-1/5 bg-gray-400 h-12">
+            <h1>hello</h1>
+        </div>
+        <div class="w-1/3 bg-gray-500 h-12">
+            <Canvas/>
+        </div>
+        <div class="w-1/5 bg-gray-400 h-12">
+            <Toolbox/>
+        </div>
+    </div>
+</main>
