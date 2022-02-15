4a65953b70fb9794f1d05c404d590bdac53d2bb5
Philippos Slicher <philipposslicher@gmail.com>
2022-02-15 16:25:00 +0100

    feat: Initialize backend project (node, Babel, ESLint, Jest)

---
+++ packages/backend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'no-console': 'off',
+  },
+};
