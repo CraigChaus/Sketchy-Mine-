c9353fd16c7aa43e4dc699fd4281a3529b722be6
PeterP_Saxion <490498@student.saxion.nl>
2022-02-16 17:52:17 +0100

    feat: Add ChatBox svelte component +feat: Add ChatButton svelte component +feat: Add TextInput svelte component

---
+++ packages/frontend/src/components/chat/ChatBox.svelte
@@ -0,0 +1,26 @@
+<script>
+    import ChatItem from "./ChatItem.svelte";
+    /*
+     Expected content:
+     [
+         {
+             "username":"bob",
+             "message":"Hello everyone!",
+             "type": 1 (self) or 2 (message) or 3 (system)
+         }
+     ]
+     */
+    export let contentJSON = [];
+</script>
+
+<div class="border-4 h-80 overflow-y-auto max-w-2xl flex flex-col">
+    {#if contentJSON !== "" || contentJSON !== undefined || contentJSON !== null}
+        {#each contentJSON as element}
+            <ChatItem
+                sender={element.username}
+                message={element.message}
+                type={element.type}
+            />
+        {/each}
+    {/if}
+</div>
---
+++ packages/frontend/src/components/chat/ChatButton.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let styles = "";
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    export let buttonClicked = () => dispatch ("buttonClicked");
+
+</script>
+
+<div class="bg-green-600 rounded-lg border-none text-white p-2 text-center text inline-block transition-all cursor-pointer {styles}" on:click={buttonClicked}>
+    <p><slot/></p>
+</div>
+
+<style>
+  div:hover{
+        filter: brightness(0.85);
+    }
+    div:hover.disabled{
+        filter: brightness(0.50);
+    }
+    div:hover p:after {
+        opacity: 1;
+        right: 0;
+    }
+</style>
