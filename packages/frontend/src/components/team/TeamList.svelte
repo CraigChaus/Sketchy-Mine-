42603cbca43d53a89eb69f8929dc0881999d5012
PeterP_Saxion <490498@student.saxion.nl>
2022-02-21 15:56:51 +0100

    feat: Add basic TeamList component

---
+++ packages/frontend/src/components/team/TeamList.svelte
@@ -0,0 +1,28 @@
+<script>
+import TeamItem from "./TeamItem.svelte";
+
+    /*
+       Expected content:
+       [
+           {
+               "teamname":"Team 1",
+               "isDrawing": false,
+               "isSelf": true,
+               "won": true,
+               "placementNr": 4
+           }
+       ]
+       */
+    export let contentJSON = [];
+    export let showResults = false;
+
+  </script>
+  
+  <div class="border-4 max-h-96 overflow-y-auto flex flex-col flex-1">
+    {#if contentJSON !== "" || contentJSON !== undefined || contentJSON !== null}
+      {#each contentJSON as element}
+        <TeamItem showResults={showResults} isSelf={element.isSelf} isDrawingTeam={element.isDrawing} won={element.won} placementNr={element.placementNr}>{element.teamname}</TeamItem>
+      {/each}
+    {/if}
+  </div>
+  
