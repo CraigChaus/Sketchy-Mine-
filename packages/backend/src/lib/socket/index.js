d64ddfe9c18d039f87e762480a4f98ed1ef1e1b4
Melissa Neamt <498265@student.saxion.nl>
2022-02-25 13:57:44 +0100

    feat: Add canvas handler

--- packages/backend/src/lib/socket/index.js
+++ packages/backend/src/lib/socket/index.js
@@ -1,15 +1,22 @@
 import { Server } from 'socket.io';
 import chatHandler from './handlers/chatHandler';
+import canvasHandler from './handlers/canvas-handler';
 
 const debug = require('debug')('socket');
 
 const setup = (server) => {
   debug('Initializing Socket.IO server ');
-  const io = new Server(server, { /* options */ });
+  const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:8080",
+      methods: ["GET", "POST"],
+    }
+  });
 
   const onConnection = (socket) => {
     debug('New socket connection');
     chatHandler(io, socket);
+    canvasHandler(io, socket);
   };
 
   io.on('connection', onConnection);
