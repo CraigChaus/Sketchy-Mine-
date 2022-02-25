0f872f3383d087f4ff46b9c438be8c1d6ee69fa1
Melissa Neamt <498265@student.saxion.nl>
2022-02-25 13:56:43 +0100

    fix: fix Main component after merging

--- packages/frontend/src/pages/Main.svelte
+++ packages/frontend/src/pages/Main.svelte
@@ -5,8 +5,6 @@
     import TeamList from "../components/team/TeamList.svelte";
     import MessageBar from "../components/chat/MessageBar.svelte";
     import Toolbox from "../Canvas/Toolbox.svelte";
-    import Chat from "./Chat.svelte";
-    import PageLayout from "../components/PageLayout/PageLayout.svelte";
 
     let teams = [
         {
@@ -123,54 +121,26 @@
     function clear() {
         SDraw.clearDrawings();
     }
-
 </script>
-<PageLayout>
-    <div class="p-4">
-        <div class="flex mb-4">
-            <div class="w-1/3 h-full bg-gray-300">
-                <p>teams go here. teams go here. teams go here. teams go here. teams go here. teams go here. teams go
-                    here.
-                    teams go here. teams go here. teams go here. teams go here. teams go here. teams go here. teams go
-                    here.
-                    teams go here. teams go here. teams go here. teams go here. teams go here. teams go here. teams go
-                    here.
-                    teams go here. teams go here. teams go here. teams go here. teams go here. teams go here. teams go
-                    here.
-                    teams go here. teams go here. teams go here. teams go here. teams go here. teams go here. teams go
-                    here.
-                    teams go here. teams go here. teams go here. teams go here. teams go here. teams go here. teams go
-                    here.
-                    teams go here. teams go here. teams go here. teams go here. teams go here. teams go here. teams go
-                    here.
-                    teams go here. teams go here. teams go here. teams go here. teams go here. teams go here. teams go
-                    here.
-                </p>
-            </div>
-            <div class="flex my-8">
-                <div class="w-1/3 h-12 m-3">
-                    <GuessList teamNumber={1} {currentGuess}/>
-                    <TeamList showResults={true} contentJSON={teams}/>
-                </div>
 
-                <div class="w-full h-full">
-                    <Canvas bind:this={SDraw} {brushColor} {brushRadius} canvasWidth="640"/>
-                    <MessageBar
-                            bind:input={chatInput}
-                            on:guessWordClicked={onClickGuess}
-                            on:sendChatClicked={onClickChat}
-                    />
-                </div>
-                <div class="w-2/5 h-full mx-3">
-                    <ChatBox messages={chatMessages}/>
-                </div>
-                <div class="flex-row justify-center">
-                    <Toolbox bind:SDraw={SDraw} bind:brushColor={brushColor} bind:brushRadius={brushRadius}/>
-                </div>
-                <div class="w-1/3 p-2 m-4 justify-center">
-                    <Chat/>
-                </div>
-            </div>
-        </div>
+<div class="flex my-8">
+    <div class="w-1/3 h-12 m-3">
+        <GuessList teamNumber={1} {currentGuess}/>
+        <TeamList showResults={true} contentJSON={teams}/>
     </div>
-</PageLayout>
+
+    <div class="w-full h-full">
+        <Canvas bind:this={SDraw} {brushColor} {brushRadius} canvasWidth="640"/>
+        <div class="flex-row justify-center">
+            <Toolbox bind:SDraw={SDraw} bind:brushColor={brushColor} bind:brushRadius={brushRadius}/>
+        </div>
+        <MessageBar
+                bind:input={chatInput}
+                on:guessWordClicked={onClickGuess}
+                on:sendChatClicked={onClickChat}
+        />
+    </div>
+    <div class="w-2/5 h-full mx-3">
+        <ChatBox messages={chatMessages}/>
+    </div>
+</div>
