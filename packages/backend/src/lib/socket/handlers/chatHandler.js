000519ebfb8487cbf7c6996706b7b5e7edcf4dd6
Philippos Slicher <philipposslicher@gmail.com>
2022-03-03 21:20:21 +0100

    chore: Cleanup and modernify chat backend code

--- packages/backend/src/lib/socket/handlers/chatHandler.js
+++ packages/backend/src/lib/socket/handlers/chatHandler.js
@@ -1,43 +1,41 @@
-const messageFormat = require('../utils/messages').default;
-
-const {
+import {
   userJoin, getCuurentUser, userLeave, getSessionUsers,
-} = require('../utils/users');
+} from '../utils/users';
+
+import messageFormat from '../utils/messages';
 
 // This will appear as the name of the sender
 const name = 'Sketchy Mine Bot';
 
-/** const CHAT_EVENTS = {
+const CHAT_EVENTS = {
+  JOIN: 'joinSession',
   SEND: 'chat:send',
-}; */
+  MESSAGE: 'message',
+  SESSION_USERS: 'sessionUsers',
+  CHAT_MESSAGE: 'chatMessage',
+};
 
 const chatHandler = (io, socket) => {
-  /** const sendChat = (payload) => {
-    console.log(`Received: ${payload}`);
-    // io.emit(CHAT_EVENTS.SEND);
-  }; */
-  // socket.on(CHAT_EVENTS.SEND, sendChat);
-
-  socket.on('joinSession', ({ username, session }) => {
+  socket.on(CHAT_EVENTS.JOIN, ({ username, session }) => {
     const user = userJoin(socket.id, username, session);
     socket.join(user.session);
     // Welcome current user
-    socket.emit('message', messageFormat(name, 'Welcome to Sketchy Mine!'));
+    socket.emit(CHAT_EVENTS.MESSAGE, messageFormat(name, 'Welcome to Sketchy Mine!'));
 
     // Send to all users this message except for current user
-    socket.broadcast.to(user.session).emit('message', messageFormat(name, `${user.username} has joined the chat`));
+    socket.broadcast.to(user.session).emit(CHAT_EVENTS.MESSAGE, messageFormat(name, `${user.username} has joined the chat`));
 
     // Send users and session info
-    io.to(user.session).emit('sessionUsers', {
+    io.to(user.session).emit(CHAT_EVENTS.SESSION_USERS, {
       room: user.session,
       users: getSessionUsers(user.session),
     });
   });
 
   // Listen for chat message
-  socket.on('chatMessage', (msg) => {
+  socket.on(CHAT_EVENTS.CHAT_MESSAGE, (msg) => {
     const user = getCuurentUser(socket.id);
-    io.to(user.session).emit('message', messageFormat(user.username, msg));
+    io.to(user.session).emit(CHAT_EVENTS.MESSAGE, messageFormat(user.username, msg));
   });
 
   // Runs when client disconnects from the server
@@ -45,11 +43,11 @@ const chatHandler = (io, socket) => {
     const user = userLeave(socket.id);
     if (user) {
       // Send to everyone using emit() method
-      io.to(user.session).emit('message', messageFormat(name, `${user.username} has left the chat`));
+      io.to(user.session).emit(CHAT_EVENTS.MESSAGE, messageFormat(name, `${user.username} has left the chat`));
     }
 
     // Send users and session info again when user disconnects
-    io.to(user.session).emit('sessionUsers', {
+    io.to(user.session).emit(CHAT_EVENTS.SESSION_USERS, {
       room: user.session,
       users: getSessionUsers(user.session),
     });
