4bb999c9493444a8900edce5261280d172eb704e
Melissa Neamt <498265@student.saxion.nl>
2022-03-09 13:55:51 +0100

    feat: Add GET user by ID request

--- packages/backend/src/routes/users.js
+++ packages/backend/src/routes/users.js
@@ -1,10 +1,29 @@
 import { Router } from 'express';
 
+const { StatusCodes } = require('http-status-codes');
+
 const router = Router();
+let users = require('../data/users');
 
 /* GET users listing. */
 router.get('/', (req, res) => {
-  res.send('respond with a resource');
+  res.send(users);
+});
+
+/* Get user by ID */
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const user = users.find((user) => {
+    return user.id == id;
+  });
+
+  if (user) {
+    res.send(user);
+  } else {
+    res
+        .status(StatusCodes.NOT_FOUND)
+        .send(`User with id ${id} not found`);
+  }
 });
 
 export default router;
