a1ce64649c5152836b04258ba8f5531754d5488d
Philippos Slicher <philipposslicher@gmail.com>
2022-03-09 13:44:29 +0100

    chore: Abstract joinMatch function

--- packages/frontend/src/pages/Main.svelte
+++ packages/frontend/src/pages/Main.svelte
@@ -163,14 +163,17 @@
   onMount(() => {
     username = `User${Math.round(Math.random() * 10000)}`;
     socket.emit("joinSession", { username, session }, () => {
-      console.log("Joined session");
       socket.emit("teams:get");
-
-      socket.emit("teams:join", "Team 1"); //TODO: This should only run on matchmaking
+      // joinMatch();
     });
   });
 
   teamsValue.set(teams);
+
+  const joinMatch = () => {
+    socket.emit("teams:get");
+    socket.emit("teams:join", "Team 1"); //TODO: This should only run on matchmaking
+  };
 
   // Receiving messages
   socket.on("message", (data) => {
