c451a1ea2381a7c5110c935e1193e6f80963da49
Philippos Slicher <philipposslicher@gmail.com>
2022-03-09 17:21:10 +0100

    chore: Comment out dummy players

--- packages/backend/src/data/teams.js
+++ packages/backend/src/data/teams.js
@@ -1,24 +1,24 @@
 import Team from './model/team';
-import User from './model/user';
+// import User from './model/user';
 
-const bob = new User('Bob');
-const jack = new User('Jack');
-const susan = new User('Susan');
-const alice = new User('Alice');
-const mark = new User('Mark');
-const david = new User('David');
+// const bob = new User('Bob');
+// const jack = new User('Jack');
+// const susan = new User('Susan');
+// const alice = new User('Alice');
+// const mark = new User('Mark');
+// const david = new User('David');
 
 const team1 = new Team('Team 1');
 
 const team2 = new Team('Team 2');
-team2.members.push(bob);
-team2.members.push(jack);
-team2.members.push(susan);
+// team2.members.push(bob);
+// team2.members.push(jack);
+// team2.members.push(susan);
 
 const team3 = new Team('Team 3');
-team3.members.push(alice);
-team3.members.push(mark);
-team3.members.push(david);
+// team3.members.push(alice);
+// team3.members.push(mark);
+// team3.members.push(david);
 
 export const Teams = [
   team1, team2, team3,
