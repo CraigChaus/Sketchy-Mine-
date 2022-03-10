33aa9f55f0fce5226751268f314c3168347a32fb
DESKTOP-TNNI89O\Patri <gri33dar@gmail.com>
2022-03-10 10:26:13 +0100

    margin for main chenged

--- packages/frontend/src/pages/Home.svelte
+++ packages/frontend/src/pages/Home.svelte
@@ -1,15 +1,15 @@
 <body class="body-bg min-h-screen pt-12 md:pt-20 pb-6 px-2 md:px-0" style="font-family:'Lato',sans-serif;">
-<main class="bg-white max-w-lg mx-auto p-8 md:p-10 my-10 rounded-lg shadow-2xl">
+<main  class="bg-white max-w-lg mx-auto p-8 md:p-12 my-3 rounded-lg shadow-2xl">
     <section>
-        <button class="text-4xl font-bold text-white text-center bg-green-400 hover:bg-green-600  py-2 px-4 rounded my-3 mx-14 w-72">Play</button>
-        <button class="text-4xl font-bold text-white text-center bg-yellow-400 hover:bg-yellow-600  py-2 px-4 rounded my-3 mx-14 w-72">Spectate</button>
-        <button class="text-4xl font-bold text-white text-center bg-pink-400 hover:bg-pink-600  py-2 px-4 rounded my-3 mx-14 w-72">Ranking</button>
-        <button class="text-4xl font-bold text-white text-center bg-red-500 hover:bg-red-700  py-2 px-4 rounded my-3 mx-14 w-72">Rules</button>
+        <button class="text-4xl font-bold text-white text-center bg-green-400 hover:bg-green-600  py-2 px-4 rounded my-3 mx-16 w-72">Play</button>
+        <button class="text-4xl font-bold text-white text-center bg-yellow-400 hover:bg-yellow-600  py-2 px-4 rounded my-3 mx-16 w-72">Spectate</button>
+        <button class="text-4xl font-bold text-white text-center bg-pink-400 hover:bg-pink-600  py-2 px-4 rounded my-3 mx-16 w-72">Ranking</button>
+        <button class="text-4xl font-bold text-white text-center bg-red-500 hover:bg-red-700  py-2 px-4 rounded my-3 mx-16 w-72">Rules</button>
     </section>
 
     <section class="mt-10">
-        <button class="text-4xl font-bold text-white text-center bg-blue-400 hover:bg-blue-600  py-2 px-4 rounded my-3 mx-14 w-72">Log in</button>
-        <button class="text-4xl font-bold text-white text-center bg-blue-700 hover:bg-blue-900  py-2 px-4 rounded my-3 mx-14 w-72">Register</button>
+        <button class="text-4xl font-bold text-white text-center bg-blue-400 hover:bg-blue-600  py-2 px-4 rounded my-3 mx-16 w-72">Log in</button>
+        <button class="text-4xl font-bold text-white text-center bg-blue-700 hover:bg-blue-900  py-2 px-4 rounded my-3 mx-16 w-72">Register</button>
     </section>
 </main>
 </body>
