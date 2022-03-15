8fdcf877340dc886f3512303eb17e461df626419
mirai814 <Top10Tupismi#3>
2022-03-15 18:57:24 +0100

    feature: ended session screen

--- packages/frontend/src/pages/EndedSession.svelte
+++ packages/frontend/src/pages/EndedSession.svelte
@@ -3,8 +3,8 @@
 
 </script>
 
-<div>
-    <h1 class="">SESSION ENDED</h1>
-    <h3 class=""> {reason} </h3>
-    <h3 class="">Your progress until now has been saved</h3>
+<div class="grid place-items-center">
+    <h1 class="h-12 m-10">SESSION ENDED</h1>
+    <h3 class="h-12"> {reason} </h3>
+    <h3 class="h-12">Your progress until now has been saved</h3>
 </div>
