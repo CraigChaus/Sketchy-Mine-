a4a1278235b0b0b01873d7a125afdd760fe94439
Melissa Neamt <498265@student.saxion.nl>
2022-03-16 11:38:42 +0100

    chore: Refactor credentials and users routes

--- packages/backend/src/routes/credentials.js
+++ packages/backend/src/routes/credentials.js
@@ -1,41 +1,7 @@
-const express = require('express');
-const { StatusCodes } = require('http-status-codes');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const users = require('../data/users');
+import express from 'express';
+import create from '../database/controllers/credentials_controller';
 
 const router = express.Router();
+router.post('', create);
 
-const login = (username, password) => {
-  if (username && password) {
-    const user = users.find((foundUser) => foundUser.username === username);
-    if (user) {
-      const result = bcrypt.compareSync(password, user.password);
-      if (result) {
-        return jwt.sign({
-          id: user.id,
-          username: user.username,
-          roles: user.roles,
-        }, user.secret);
-      }
-    }
-    return false;
-  }
-  return false;
-};
-
-router.post('', (req, res) => {
-  const { username, password } = req.body;
-  if (username && password) {
-    const token = login(username, password);
-    if (token) {
-      res.send({ token, user: jwt.decode(token) });
-    } else {
-      res.status(StatusCodes.UNAUTHORIZED).send('Username and/or password incorrect!');
-    }
-  } else {
-    res.status(StatusCodes.BAD_REQUEST).send('Required parameter missing!');
-  }
-});
-
-module.exports = router;
+export default router;
--- packages/backend/src/routes/users.js
+++ packages/backend/src/routes/users.js
@@ -1,71 +1,22 @@
 import { Router } from 'express';
-
-const { StatusCodes } = require('http-status-codes');
-const { uuid } = require('uuidv4');
-const bcrypt = require('bcrypt');
-const isLoggedIn = require('../middleware/is-logged-in');
+import isLoggedIn from '../middleware/is_logged_in';
+import * as users from '../database/controllers/user_controller';
 
 const router = Router();
-const users = require('../data/users');
 
-/* GET users listing. */
-router.get('/', isLoggedIn, (req, res) => {
-  res.send(users);
-});
+/* Get users listing */
+router.get('/', isLoggedIn, users.findAll);
 
 /* Get user by ID */
-router.get('/:id', isLoggedIn, (req, res) => {
-  const { id } = req.params;
-  const user = users.find((us) => us.id === id);
-
-  if (user) {
-    res.send(user);
-  } else {
-    res
-      .status(StatusCodes.NOT_FOUND)
-      .send(`User with id ${id} not found`);
-  }
-});
+router.get('/:id', isLoggedIn, users.findById);
 
 /* Register user */
-router.post('/', isLoggedIn, async (req, res) => {
-  try {
-    console.log(req.body);
-    users.push({
-      id: users.length + 1,
-      username: req.body.username,
-      password: await bcrypt.hash(req.body.password, 12),
-      secret: uuid(),
-    });
+router.post('/', users.create);
 
-    res
-      .status(StatusCodes.CREATED)
-      .send('User registered!');
-  } catch {
-    res
-      .status(StatusCodes.BAD_REQUEST)
-      .send('Please fill in username/password');
-  }
-});
+/* Update User by id */
+router.put('/:id', isLoggedIn, users.update);
 
-router.post('/register', async (req, res) => {
-  try {
-    users.push({
-      id: users.length + 1,
-      username: req.body.username,
-      password: await bcrypt.hash(req.body.password, 10),
-      secret: uuid(),
-      roles: ['user'],
-    });
-
-    res
-      .status(StatusCodes.CREATED)
-      .send('User registered!');
-  } catch {
-    res
-      .status(StatusCodes.BAD_REQUEST)
-      .send('Please fill in username/password');
-  }
-});
+/* Delete User by id */
+router.delete('/:id', isLoggedIn, users.deleteUser);
 
 export default router;
