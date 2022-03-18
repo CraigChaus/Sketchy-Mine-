d70343297e48af6f82ca7a6412fdf54848e09689
Philippos Slicher <philipposslicher@gmail.com>
2022-03-18 20:06:35 +0100

    feat: Add Sentry tracing and monitoring integration to the frontend

--- packages/frontend/src/main.js
+++ packages/frontend/src/main.js
@@ -1,4 +1,20 @@
+import * as Sentry from '@sentry/browser';
+import { BrowserTracing } from '@sentry/tracing';
 import App from './App.svelte';
+
+const { env } = process;
+
+const { SENTRY_DSN, IS_PROD } = env;
+
+if (IS_PROD && SENTRY_DSN) {
+  // eslint-disable-next-line no-console
+  console.log('Configuring production tracing and logging');
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
 
 const app = new App({
   target: document.body,
