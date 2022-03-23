61c373b32a2f5d664765fe1334c9b7a657c31ebb
Philippos Slicher <philipposslicher@gmail.com>
2022-03-23 14:43:14 +0100

    feat: Display team level

--- packages/frontend/src/components/team/TeamList.svelte
+++ packages/frontend/src/components/team/TeamList.svelte
@@ -34,7 +34,18 @@
                 isSelf={element.isSelf}
                 isDrawingTeam={element.isDrawing}
                 won={element.won}
-                placementNr={element.placementNr}>{element.teamname}</TeamItem
+                placementNr={element.placementNr}>
+                <div class="flex flex-row">
+                    <p>
+                        {element.teamname}
+                    </p>
+                    {#if element.teamname !== "Spectators"}
+                        <p class="ml-auto font-semibold text-blue-100 mr-1">
+                            Level {element.level}
+                        </p>
+                    {/if}
+                </div>
+                </TeamItem
             >
         {/each}
     {/if}
