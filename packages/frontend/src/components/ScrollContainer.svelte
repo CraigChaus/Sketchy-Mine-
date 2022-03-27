21d5cb5eced334db2814eb83c668e825d0eb654f
PeterP_Saxion <490498@student.saxion.nl>
2022-03-27 13:35:54 +0200

    fix: Implement chat autoscrolling

--- packages/frontend/src/components/ScrollContainer.svelte
+++ packages/frontend/src/components/ScrollContainer.svelte
@@ -3,8 +3,12 @@
      * Component to implement scroll bar on a div element
      */
     export let styles;
+    export let container;
 </script>
 
-<div class="max-h-96 overflow-y-auto flex flex-col flex-1 {styles}">
+<div
+    class="max-h-96 overflow-y-auto flex flex-col flex-1 {styles}"
+    bind:this={container}
+>
     <slot />
 </div>
