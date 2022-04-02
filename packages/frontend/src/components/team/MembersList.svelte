a5bcad82d269d0d4999205a34f1193dbe917deb5
DESKTOP-TNNI89O\Patri <gri33dar@gmail.com>
2022-04-02 21:47:22 +0200

    no idea how to test it. Request to delete user from team

--- packages/frontend/src/components/team/MembersList.svelte
+++ packages/frontend/src/components/team/MembersList.svelte
@@ -3,6 +3,7 @@
    * MembersList component
    * A dropdown list under each team showing players per team
    */
+  import { token as tokenStore } from "../../stores/token";
   import Icon from "svelte-awesome";
   import {
     faSpellCheck,
@@ -15,6 +16,26 @@
 
   export let membersJSON = [{ username: "N/A", guessed: false }]; // Default data
   export let showGuessedIndicator = true; // Used to indicate if someone submitted a guess from the team
+
+  import { API_URL } from "../../socket";
+  // // import { token as tokenStore } from "../stores/token";
+  //
+  //
+
+  export async function removePlayer(username) {
+      const res = await fetch(API_URL + '/users/' + username, {
+          method: 'DELETE',
+          headers:{
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${$tokenStore}`,
+          }
+      });
+
+      if (!res.ok) throw new Error(await res.text());
+  }
+
+
+
 </script>
 
 <div class="bg-gray-100 mb-2 rounded-bl-lg p-2 flex-1 ml-5 divide-y">
@@ -34,7 +55,8 @@
 
       {#if $user.is_moderator}
         <p class="flex justify-end ">
-          <button
+<!--          <button>-->
+          <button    on:click={() => removePlayer(member.username)}
             class="flex justify-center h-6 w-6 bg-yellow-300 hover:bg-yellow-500 font-bold  rounded-full"
             ><Icon data={faPersonRunning} scale="1.4" style="color:black" />
           </button>
