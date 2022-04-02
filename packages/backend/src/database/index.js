2b736f16096f986df358c6764355f0e9afa715fa
PeterP_Saxion <490498@student.saxion.nl>
2022-04-02 14:15:20 +0200

    feat: Setup database model for wordbank

--- packages/backend/src/database/index.js
+++ packages/backend/src/database/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import debug from 'debug';
 import User from './controllers/models/user_model';
+import Word from './controllers/models/word_model';
 // import WordBank from './models/word_bank';
 import { IS_PROD } from '../app';
 import sequelize from './util/config';
@@ -13,8 +14,8 @@ const setupDatabase = async () => {
     dbg('Database connection successful.');
     await User.sync({ force: (process.env.SYNC === 'true') ?? !IS_PROD });
     dbg('User table created/updated.');
-    // await WordBank.sync();
-    // dbg('Words table created/updated.');
+    await Word.sync();
+    dbg('Word table created/updated.');
   } catch (error) {
     console.error(error);
   }
