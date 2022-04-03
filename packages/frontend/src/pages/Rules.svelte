a334af92967db59ee944ae38b0ec29810410c24e
CraigChaus <craigchauraya@gmail.com>
2022-04-03 14:33:16 +0200

    feat: Add rules and gameplay page

---
+++ packages/frontend/src/pages/Rules.svelte
@@ -0,0 +1,47 @@
+<script>
+</script>
+
+<div class = "background">
+    <body class="bg-gray-100 font-family-karla flex body-bg min-h-screen">
+    <div class="w-screen mt-12 m-96">
+        <h3 class="text-center font-bold p-2 text-4xl">Rules and Gameplay</h3>
+        <div class="bg-white overflow-auto">
+            <h2 class="text-center font-bold p-2 underline text-2xl">Gameplay</h2>
+            <h2 class="text-center p-2 underline" >Drawing Team</h2>
+            <p class="text-left p-2">As the drawing team you are meant to draw on the canvas a word that is randomly given to you by the game within a
+                displayed amount of time.
+                You must make sure the drawing is as clear as possible for the other team to guess easily. The clearer the drawing, the more points
+                you can get by allowing other teams to guess quicker. Otherwise the uglier the drawing then obviously the less points you will earn
+                cause of giving the other teams a hell of a difficult time guessing what ever you drew.</p>
+            <h2 class="text-center p-2 underline">Guessing Team</h2>
+            <p class="text-left p-2"> As the guessing team you are meant to guess what the drawing team is drawing. They will be told what to draw but you
+                wont know what they were told to draw anyway. As they are drawing you can throw in your guesses.
+
+                Up to 3 different guesses can be locked in and shown for the rest of the team to agree and click on to count it as a vote. The guess word
+                that has the most votes will be deemed as the final answer. It is only at the end of the round where the correct guess word is revealed.
+                If your guess is correct then it depends on the amount of time it took for your team to vote for the final guess word on how much points
+                you will earn. The greater the time taken the less you earn, the less time taken then the greater! And that's what you need to win ofcourse :).</p>
+            <h2 class="text-center p-2 underline">Disclaimer..sort of</h2>
+            <p class="text-left p-2 ">The drawing and guessing teams are selected at random but every team gets a taste of each role in the end :)</p>
+            <h2 class="text-center font-bold p-2 underline text-2xl">Rules</h2>
+            <ol class="text-left p-2">
+                <li>1.When drawing please do not draw the word itself. e.g dont draw CAR as CAR on canvas</li>
+                <li>2.No foul language in the chat or guesses board will be tolerated</li>
+                <li>3.As mentioned before, the teams are selected at random but your turn will come, so be patient and don't rage in chat</li>
+                <li>4.Do not draw offensive or explicit content ofcourse</li>
+                <li>5.Be nice to everyone, ALWAYS!!.</li>
+            </ol>
+            <p class="text-left p-2">Failure to abide by the rules will lead to first and final warning from the moderator and if the devil in you persists
+                on going against the rules then you will just get permanently banned.</p>
+
+            <div class = "text-center p-2">
+                <a href="/" class = " center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded p-5">GO AND HAVE FUN</a>
+            </div>
+
+        </div>
+    </div>
+</body>
+</div>
+
+<style>
+</style>
